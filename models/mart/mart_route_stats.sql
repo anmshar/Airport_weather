6363620a7e5c6d2008c6c8bcf038ff63f9b83843
anmol sharma <anmolsharma0126@gmail.com>
2024-11-12 14:33:09 +0100

    hello

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -1,5 +1,5 @@
 with rout as(
-select distinct tail_number,origin,
+select distinct tail_number,origin as faa,
 round(avg(dep_delay),2) as avrage_dep_delay,
 round(avg(actual_elapsed_time),2)as average_elapsed ,
 round(avg(arr_delay),2)as average_arr_delay,
@@ -8,11 +8,11 @@ sum(diverted) as no_diverted_flights,
 airline,
 dest,
 count(*) as total_numbers_of_flight
-from {{ref('prep_flights')}}
-group by tail_number ,airline ,dest ,origin)
+from {{ref('prep_flights')}} 
+group by tail_number ,airline ,dest ,faa)
 select r.*,
 pa.name,
 pa.country,
 r.dest
 from rout r  
-join {{ref('prep_airports')}} pa on r.origin = pa.faa
+join {{ref('prep_airports')}} pa on r.faa = pa.faa
