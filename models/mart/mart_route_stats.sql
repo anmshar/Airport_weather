27f88194bf7cb3e99ead953f9b0497879322c7a8
anmol sharma <anmolsharma0126@gmail.com>
2024-11-12 14:24:29 +0100

    hello

---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,18 @@
+with rout as(
+select distinct tail_number,origin,
+round(avg(dep_delay),2) as avrage_dep_delay,
+round(avg(actual_elapsed_time),2)as average_elapsed ,
+round(avg(arr_delay),2)as average_arr_delay,
+sum(cancelled)as canscelled_flights ,
+sum(diverted) as no_diverted_flights,
+airline,
+dest,
+count(*) as total_numbers_of_flight
+from {{ref('prep_flights')}}
+group by tail_number ,airline ,dest ,origin),
+select r.*,
+pa.name,
+pa.country,
+r.dest
+from rout r  
+join {{ref('prep_airports')}} pa on r.origin = pa.faa
